200885da2c28846d5ef70224562fe89d9f4b1e22
jrwang8 <47158965+jrwang8@users.noreply.github.com>
2019-04-26 23:02:21 -0400

    Update MySQLDBUtil.java

--- src/db/mysql/MySQLDBUtil.java
+++ src/db/mysql/MySQLDBUtil.java
@@ -10,6 +10,4 @@ public class MySQLDBUtil {
 			+ HOSTNAME + ":" + PORT_NUM + "/" + DB_NAME
 			+ "?user=" + USERNAME + "&password=" + PASSWORD
 			+ "&autoReconnect=true&serverTimezone=UTC";
-	//这里我把端口从8889改成3306，password改成空了
-	//这里为了配置到aws上，password改回了root，因为aws的mysql密码就是root
 }
